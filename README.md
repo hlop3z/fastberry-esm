834e483bcdeb4e2f0555391d7656a5ba5a1f4d5c
hlop3z <23062270+hlop3z@users.noreply.github.com>
2023-01-10 15:58:57 -0600

    v0.2.5

--- README.md
+++ README.md
@@ -85,4 +85,91 @@ console.log(graphql.task.args.Search);
 // Get OPERATION { Form } Vue-Reactive
 console.log("Form");
 console.log(graphql.task.form.Search);
+
+// Demo (Item & List)
+graphql.task.item.current = {
+  id: "Mzo6NGQ0MDk4MzIxNjU0YTQ1Nw==",
+  title: "First App",
+  description: "create an example app.",
+  status: "open",
+};
+
+graphql.task.item.current = {
+  id: "Mjo6M2VmOWFiYmI1ZGY1YjY0MQ==",
+  title: "Post Tutorial",
+  description: "post the example app.",
+  status: "open",
+};
+
+// Item (Object)
+console.log(graphql.task.item.loading);
+console.log(graphql.task.item.current);
+console.log(graphql.task.item.last);
+console.log(graphql.task.item.history);
+
+// List (Array)
+console.log(graphql.task.list.pagination);
+
+// Search (Array)
+console.log(graphql.task.search.history);
+```
+
+<br>
+
+## Core
+
+```js
+/*
+  @ Example With => { Vite }
+*/
+import TaskGQL from "./Task.graphql?raw";
+import fastberry from "./fastberry";
+
+// Start Backend
+const Admin = fastberry();
+
+// (GraphQL) Start Manager
+const graphql = Admin.models({
+  task: TaskGQL,
+});
+```
+
+<br>
+
+## Fields for Model
+
+| Property      | Type   | Definition                              |
+| ------------- | ------ | --------------------------------------- |
+| **`methods`** | Array  | All { Operations } in the GraphQL Query |
+| **`form`**    | Object | All { Forms } for each Operation        |
+| **`args`**    | Object | All { Args } for each Operation         |
+| **`item`**    | Object | Single Instance Manager                 |
+| **`list`**    | Object | Default List Manager                    |
+| **`search`**  | Object | Search List Manager                     |
+
+### Example Usage
+
+```js
+graphql.{ MODEL_NAME }.methods
+```
+
+<br>
+
+## Fields for [ Item, List, Search ]
+
+| Property         | Type    | Definition               |
+| ---------------- | ------- | ------------------------ |
+| **`current`**    | Any     | Current value            |
+| **`last`**       | Any     | Last value               |
+| **`history`**    | Array   | Last 3 values            |
+| **`pagination`** | Object  | Pagination Form          |
+| **`loading`**    | Boolean | Object is Loading?       |
+| **`error`**      | Object  | Error Information        |
+| **`info`**       | Object  | Current Page Information |
+| **`selected`**   | Array   | Selected Item(s)         |
+
+### Example Usage
+
+```js
+graphql.{ MODEL_NAME }.item.current
 ```
--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,101 @@
 import API from "./api";
 
+/**
+ * Model Part Handler
+ * @property {Any} current        Current value
+ * @property {Any} last           Last value
+ * @property {Array} history      Last 3 values
+ * @property {Object} pagination  Pagination Form
+ * @property {Boolean} loading    Object is Loading?
+ * @property {Object} error       Error Information
+ * @property {Object} info        Current Page Information
+ * @property {Array} selected     Selected ITEM(s)
+ */
+class SchemaList {
+  constructor(list = false, maxSize = 3) {
+    // Max-Size
+    this._maxSize = maxSize;
+
+    // Reactive Dict
+    const dict = {
+      history: [],
+      current: null,
+      loading: true,
+      error: {},
+    };
+
+    if (list) {
+      dict.info = {};
+      dict.selected = [];
+      dict.pagination = reactive({
+        page: 1,
+        limit: 50,
+        sortBy: "-id",
+        all: false,
+      });
+    }
+
+    // Init Reactive
+    this._ = reactive(dict);
+  }
+
+  // Setters Only
+  setbyAPI(value) {
+    this._.current = value;
+    this._.history.push(value);
+    const size = this._.history.length;
+    if (size > this._maxSize) {
+      this._.history.shift();
+    }
+  }
+
+  // Getters Only
+  get last() {
+    return this._.history[this._.history.length - 2];
+  }
+  get history() {
+    return this._.history;
+  }
+  get pagination() {
+    return this._.pagination;
+  }
+  get selected() {
+    return this._.selected;
+  }
+
+  // Setters & Getters
+  set current(value) {
+    return this.setbyAPI(value);
+  }
+  get current() {
+    return this._.current;
+  }
+  get loading() {
+    return this._.loading;
+  }
+  set loading(value) {
+    this._.loading = value;
+  }
+  get error() {
+    return this._.error;
+  }
+  set error(value) {
+    this._.error = value;
+  }
+  get info() {
+    return this._.info;
+  }
+  set info(value) {
+    this._.info = value;
+  }
+}
+
+/**
+ * GraphQL Model Manager
+ * @property {Object} api      Backend API
+ * @property {Function} models Create GraphQL Models
+ * @property {Object} graphql  Manage GraphQL Models
+ */
 class Manager {
   constructor(url = "http://localhost:8000", options) {
     this.api = API(url, options);
@@ -18,6 +114,9 @@ class Manager {
       form: {},
       args: {},
       ops: {},
+      item: new SchemaList(false),
+      list: new SchemaList(true),
+      search: new SchemaList(true),
     };
     admin.op.$keys.forEach((key) => {
       setup.ops[key] = (props) => admin.crud(key, props);
@@ -35,6 +134,15 @@ class Manager {
     return this._graphql[name];
   }
   models(obj) {
+    /**
+     * GraphQL Model Manager
+     * @property {Array} methods  All { Operations } in the GraphQL Query
+     * @property {Object} form    All { Forms } for each Operation
+     * @property {Object} args    All { Args } for each Operation
+     * @property {Object} item    Single Instance Manager
+     * @property {Object} list    List Manager
+     * @property {Object} search  Search Manager
+     */
     Object.keys(obj).forEach((key) => {
       const val = obj[key];
       this.model(key, val);
